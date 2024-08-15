f206260d00152abe94df4b880b2fab7903135744
mukut1994 <mukut1994@gmail.com>
2024-08-15 19:52:05 +0100

    Fix missing error for AlibreX.dll

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace CheckAlibreXPortableBinDirIsWorking // Define a namespace for our appl
             IADRoot root = (IADRoot)hook.Root; // Retrieve the root object from the hook cast to IADRoot interface
             string exeDir = AppDomain.CurrentDomain.BaseDirectory; // Get the directory where the executable is running
             string filePath = Path.Combine(exeDir, "test.AD_PRT"); // Combine the executable directory with the filename to form the full path
-            IADSession session = root.OpenFile(filePath); // Open a file using the root object and obtain a session
+            IADSession session = root.OpenFile("C:/Users/shaon/Desktop/github/CheckAlibreXPortableBinDirIsWorking/test.AD_PRT"); // Open a file using the root object and obtain a session
             Console.WriteLine(root.AppTitle); // Print the application title to the console
             Console.WriteLine(session.FilePath); // Print the path of the currently opened file
             Console.WriteLine(session.Name); // Print the name of the session
